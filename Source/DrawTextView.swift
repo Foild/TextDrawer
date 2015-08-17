a124604ae51c672f4775dd1b7d4720b6c85b54c4
bevbomb <grantlbevan@icloud.com>
2015-08-17 16:59:04 +1000

    Added new features to add multiple labels and included delete for hover

--- Source/DrawTextView.swift
+++ Source/DrawTextView.swift
@@ -1,6 +1,6 @@
 //
 //  DrawTextView.swift
-//  
+//
 //
 //  Created by Remi Robert on 11/07/15.
 //
@@ -15,48 +15,94 @@ class CustomLabel: UILabel {
 }
 
 public class DrawTextView: UIView {
-
-    var textLabel: CustomLabel!
+    
+    var labels = NSMutableArray()
+    var currentLabel: CustomLabel?
     
     var text: String! {
         didSet {
-            textLabel.text = text
-            sizeTextLabel()
+            if let current = currentLabel {
+                current.text = text
+                self.sizeTextLabel(current)
+            }
         }
     }
-
+    
     init() {
         super.init(frame: CGRectZero)
-        
         layer.masksToBounds = true
         backgroundColor = UIColor.clearColor()
-        textLabel = CustomLabel()
-        textLabel.font = textLabel.font.fontWithSize(44)
-        textLabel.textAlignment = NSTextAlignment.Center
-        textLabel.numberOfLines = 0
-        textLabel.textColor = UIColor.blackColor()
-        textLabel.backgroundColor = UIColor.clearColor()
-        addSubview(textLabel)
+    }
+    
+    func addLabel(completion: ((label: UILabel) -> Void)? = nil) {
+        var label = CustomLabel()
+        label.text = "Enter text"
+        label.font = label.font.fontWithSize(44)
+        label.textAlignment = NSTextAlignment.Center
+        label.numberOfLines = 0
+        label.textColor = UIColor.blackColor()
+        label.userInteractionEnabled = true
+        label.center = self.center
+        self.sizeTextLabel(label)
         
-        textLabel.mas_makeConstraints { (make: MASConstraintMaker!) -> Void in
-            make.right.and().left().equalTo()(self)
-            make.centerY.equalTo()(self)
-            make.centerX.equalTo()(self)
+        addSubview(label)
+        
+        //        label.mas_makeConstraints { (make: MASConstraintMaker!) -> Void in
+        //            make.right.and().left().equalTo()(self)
+        //            make.centerY.equalTo()(self)
+        //            make.centerX.equalTo()(self)
+        //        }
+        
+        completion?(label: label)
+        
+        self.labels.addObject(label)
+        self.currentLabel = label
+    }
+    
+    func removeCurrentLabel() {
+        if currentLabel != nil {
+            labels.removeObject(currentLabel!)
+            currentLabel?.removeFromSuperview()
+            currentLabel = nil
         }
     }
-
-    required public init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    func removeLabel(label: CustomLabel) {
+        if labels.containsObject(label) {
+            label.removeFromSuperview()
+            labels.removeObject(label)
+            
+            if label == currentLabel {
+                currentLabel = nil
+            }
+        }
     }
-
-    func sizeTextLabel() {
+    
+    public override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let touch = touches.first as? UITouch {
+            if labels.containsObject(touch.view) {
+                if let label = touch.view as? CustomLabel {
+                    self.currentLabel = label
+                }
+            }
+        }
+    }
+    
+    func sizeTextLabel(textLabel: CustomLabel) {
+        var transform = textLabel.transform
+        textLabel.transform = CGAffineTransformIdentity
         let oldCenter = textLabel.center
         let styleText = NSMutableParagraphStyle()
         styleText.alignment = NSTextAlignment.Center
         let attributsText = [NSParagraphStyleAttributeName:styleText, NSFontAttributeName:UIFont.boldSystemFontOfSize(textLabel.font.pointSize)]
         let sizeParentView = CGSizeMake(CGRectGetWidth(superview!.frame) - 10, CGRectGetHeight(superview!.frame) - 10)
-        let sizeTextLabel = (NSString(string: textLabel.text!)).boundingRectWithSize(superview!.frame.size, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: attributsText, context: nil)
+        let sizeTextLabel = (NSString(string: textLabel.text!)).boundingRectWithSize(superview!.frame.size, options: NSStringDrawingOptions.UsesDeviceMetrics, attributes: attributsText, context: nil)
         textLabel.frame.size = CGSizeMake(sizeTextLabel.width + 10, sizeTextLabel.height + 10)
         textLabel.center = oldCenter
+        textLabel.transform = transform
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
